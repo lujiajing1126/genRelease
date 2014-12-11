d92378da59a960c233edc337e3573d5a133f1b69
megrez <lujiajing1126@gmail.com>
2014-12-11 16:45:26 +0800

    update json transfer

--- lib/genRelease/commands/help_modules/FileUtils.rb
+++ lib/genRelease/commands/help_modules/FileUtils.rb
@@ -15,7 +15,11 @@ module GenRelease
     end
 
     def format(content)
-      content.to_json.to_s
+      json_arr = []
+      content.each do |k,v|
+        json_arr << {:file => k.to_s,:md5 => v[:md5].to_s,:sha1 => v[:sha1]}
+      end
+      json_arr.to_json.to_s
     end
   end
 end
